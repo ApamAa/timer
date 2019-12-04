02285d4036339ee45e784deb0141385bccdbfa0c
ApamAa <apama.jamshidi@gmail.com>
2019-12-04 07:03:11 +0000

    main-timer2.js

--- timer2.js
+++ timer2.js
@@ -1,25 +1,21 @@
-//const args = process.argv.slice(2);
 const stdin = process.stdin;
+
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
-let makeBeep = function(){
-  process.stdout.write('\x07'); 
-}
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-stdin.on('data', (key) => {
- 
-  if (key === 'b') {
-    makeBeep();
-  }
 
+
+stdin.on('data', (key) => {
+  
+  if (key === 'b'){
+    process.stdout.write('\x07');
+  }
+  if (key >= 1 && key <= 9) {
+      console.log(`setting timer for ${key} seconds...`);
+      setTimeout(()=>{process.stdout.write('\x07')},1000*key);
+  }
   if (key === '\u0003') {
-    console.log('Thanks for using me, ciao!')
+    console.log(`Thanks for using me, ciao!`);
     process.exit();
-  } 
-  if (numbers.includes(Number(key))) {
-    const f = () => {
-      makeBeep(); 
-    }
-      setTimeout(f, key * 1000);
-  } 
+  }
 });
+
